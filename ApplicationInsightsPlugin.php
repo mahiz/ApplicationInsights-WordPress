6f3eae964852aa2792c5b65e15325a2fb33d9ec5
Alessandro Scardova <alessandro@scardova.it>
2020-05-30 10:55:35 +0200

    add Mahiz

--- ApplicationInsightsPlugin.php
+++ ApplicationInsightsPlugin.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Application Insights
-Description: Integrates a WordPress site with Microsoft Application Insights.
 Version: 2.3
-Author: ApplicationInsights
+Description: Integrates a WordPress site with Microsoft Application Insights.
+Author: ApplicationInsights - Mahiz
 License:  MIT
  */
 
